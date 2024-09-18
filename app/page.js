6ef53b387d3a76086e3368e93c58f69d7811a64f
drishya22 <drishyagarg22@gmail.com>
2024-09-18 06:31:55 +0530

    updated

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 "use client";
-import { Box, Stack } from "@mui/material";
+import { Box, Stack, TextField, Button } from "@mui/material";
 import Image from "next/image";
 import { useState } from "react";
 
@@ -12,6 +12,7 @@ export default function Home() {
   ]);
 
   const [message, setMessage] = useState("");
+
   return (
     <Box
       width="100vw"
@@ -22,15 +23,15 @@ export default function Home() {
       alignItems="center"
     >
       <Stack
-        direction="column"
-        width="600px"
+        direction={"column"}
+        width="500px"
         height="700px"
         border="1px solid black"
         p={2}
         spacing={3}
       >
         <Stack
-          direction="column"
+          direction={"column"}
           spacing={2}
           flexGrow={1}
           overflow="auto"
@@ -44,9 +45,29 @@ export default function Home() {
                 message.role === "assistant" ? "flex-start" : "flex-end"
               }
             >
-              <Box></Box>
+              <Box
+                bgcolor={
+                  message.role === "assistant"
+                    ? "primary.main"
+                    : "secondary.main"
+                }
+                color="white"
+                borderRadius={16}
+                p={3}
+              >
+                {message.content}
+              </Box>
             </Box>
           ))}
+        </Stack>
+        <Stack direction="row" spacing={2}>
+          <TextField
+            fullwidth="true"
+            label="Message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <Button variant="contained">Send</Button>
         </Stack>
       </Stack>
     </Box>
